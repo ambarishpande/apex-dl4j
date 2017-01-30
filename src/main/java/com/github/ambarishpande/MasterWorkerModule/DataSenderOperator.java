d03a3572c6eb478896febfdda504fd5c4ec64dbc
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-01-30 14:20:17 +0530

    Added temporary workaround for multiple epochs.

--- src/main/java/com/github/ambarishpande/MasterWorkerModule/DataSenderOperator.java
+++ src/main/java/com/github/ambarishpande/MasterWorkerModule/DataSenderOperator.java
@@ -1,6 +1,10 @@
 package com.github.ambarishpande.MasterWorkerModule;
 
+import java.io.IOException;
+
 import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.RawMnistDataSetIterator;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.slf4j.Logger;
@@ -20,7 +24,7 @@ public class DataSenderOperator implements InputOperator
 {
   private static final Logger LOG = LoggerFactory.getLogger(DataSenderOperator.class);
   private DataSetIterator dataSetIterator;
-
+  private int numEpochs;
   public transient DefaultOutputPort<DataSetWrapper> outputData = new DefaultOutputPort<DataSetWrapper>();
   @FieldSerializer.Bind(JavaSerializer.class)
   private DataSet d;
@@ -30,20 +34,20 @@ public class DataSenderOperator implements InputOperator
   {
     try{
       d = dataSetIterator.next();
-      LOG.info("Sending Data : " + d.toString());
+//      LOG.info("Sending Data : " + d.toString());
       DataSetWrapper dw = new DataSetWrapper(d);
       outputData.emit(dw);
 
-    }catch (IllegalArgumentException e)
+    }catch (Exception e)
     {
-      LOG.error("DataSet Complete" + e.getMessage());
-    }catch (NullPointerException e){
-      LOG.error("DataSet Complete" + e.getMessage());
+      numEpochs--;
+      if(numEpochs > 0)
+      {
+//        Temporary solution for multiple epochs
+        LOG.error("Epoch number " + numEpochs + "Complete" + e.getMessage());
+        dataSetIterator.reset();
+      }
 
-    }
-    catch (Exception e)
-    {
-      LOG.error("DataSet Complete" + e.getMessage());
     }
 
 
@@ -64,9 +68,15 @@ public class DataSenderOperator implements InputOperator
   @Override
   public void setup(Context.OperatorContext context)
   {
-    dataSetIterator = new IrisDataSetIterator(1, 150);
+    dataSetIterator = new IrisDataSetIterator(10, 150);
+//    try {
+//      dataSetIterator = new RawMnistDataSetIterator(1,100);
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+    numEpochs = 2000;
     LOG.info("Number of examples  :" + dataSetIterator.numExamples());
-    LOG.info("Iris dataset loaded...");
+    LOG.info("iris dataset loaded...");
     d = new DataSet();
 
   }
