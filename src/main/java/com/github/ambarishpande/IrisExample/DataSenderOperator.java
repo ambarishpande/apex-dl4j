8209cdae418502abf728b7aa27d59c078c48f57d
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-04-19 14:18:57 +0530

    Seperate Directory for Iris Example

---
+++ src/main/java/com/github/ambarishpande/IrisExample/DataSenderOperator.java
@@ -0,0 +1,114 @@
+package com.github.ambarishpande.IrisExample;
+
+import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.esotericsoftware.kryo.serializers.FieldSerializer;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
+import com.github.ambarishpande.MasterWorkerModule.DataSetWrapper;
+
+import com.datatorrent.api.Context;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.InputOperator;
+
+/**
+ * Created by @ambarishpande on 17/1/17.
+ */
+public class DataSenderOperator implements InputOperator
+{
+  private static final Logger LOG = LoggerFactory.getLogger(DataSenderOperator.class);
+  @FieldSerializer.Bind(JavaSerializer.class)
+  private DataSetIterator dataSetIterator;
+  private int numEpochs;
+  public transient DefaultOutputPort<DataSetWrapper> outputData = new DefaultOutputPort<DataSetWrapper>();
+  @FieldSerializer.Bind(JavaSerializer.class)
+  private DataSet d;
+  private int count;
+  private int tuplesPerWindow;
+  private int sendBatchSize;
+
+  @Override
+  public void emitTuples()
+  {
+    if (tuplesPerWindow > count) {
+      try {
+        d = dataSetIterator.next();
+//      LOG.info("Sending Data : " + d.toString());
+        DataSetWrapper dw = new DataSetWrapper(d);
+        outputData.emit(dw);
+        count++;
+      } catch (Exception e) {
+        numEpochs--;
+        if (numEpochs > 0) {
+          dataSetIterator.reset();
+        }
+
+      }
+
+    }
+
+  }
+
+  @Override
+  public void beginWindow(long l)
+  {
+    count = 0;
+  }
+
+  @Override
+  public void endWindow()
+  {
+
+  }
+
+  @Override
+  public void setup(Context.OperatorContext context)
+  {
+    dataSetIterator = new IrisDataSetIterator(sendBatchSize, 150);
+//    numEpochs = 2000;
+    LOG.info("Number of examples  :" + dataSetIterator.numExamples());
+    LOG.info("iris dataset loaded...");
+    d = new DataSet();
+
+  }
+
+  @Override
+  public void teardown()
+  {
+
+  }
+
+  public int getTuplesPerWindow()
+  {
+    return tuplesPerWindow;
+  }
+
+  public void setTuplesPerWindow(int tuplesPerWindow)
+  {
+    this.tuplesPerWindow = tuplesPerWindow;
+  }
+
+  public int getNumEpochs()
+  {
+    return numEpochs;
+  }
+
+  public void setNumEpochs(int numEpochs)
+  {
+    this.numEpochs = numEpochs;
+  }
+
+  public int getSendBatchSize()
+  {
+    return sendBatchSize;
+  }
+
+  public void setSendBatchSize(int sendBatchSize)
+  {
+    this.sendBatchSize = sendBatchSize;
+  }
+
+}
---
+++ src/main/java/com/github/ambarishpande/IrisExample/IrisExample.java
@@ -0,0 +1,92 @@
+package com.github.ambarishpande.IrisExample;
+
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import org.apache.hadoop.conf.Configuration;
+
+import com.github.ambarishpande.MasterWorkerModule.Dl4jMasterOperator;
+import com.github.ambarishpande.MasterWorkerModule.Dl4jParameterAverager;
+import com.github.ambarishpande.MasterWorkerModule.Dl4jWorkerOperator;
+import com.github.ambarishpande.MasterWorkerModule.RoundRobinStreamCodec;
+
+import com.datatorrent.api.Context;
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.annotation.ApplicationAnnotation;
+import com.datatorrent.common.partitioner.StatelessPartitioner;
+import com.datatorrent.common.util.DefaultDelayOperator;
+
+/**
+ * Created by hadoopuser on 8/4/17.
+ */
+
+@ApplicationAnnotation(name = "IrisExample")
+public class IrisExample implements StreamingApplication
+{
+  @Override
+  public void populateDAG(DAG dag, Configuration configuration)
+  {
+
+    int numWorkers = Integer.parseInt(configuration.get("dt.application.IrisExample.operator.ParameterAverager.prop.numWorkers"));
+
+    DataSenderOperator inputData = dag.addOperator("inputData", DataSenderOperator.class);
+    Dl4jMasterOperator Master = dag.addOperator("Master", Dl4jMasterOperator.class);
+    Dl4jWorkerOperator Worker = dag.addOperator("Worker", Dl4jWorkerOperator.class);
+    Dl4jParameterAverager ParameterAverager = dag.addOperator("Parameter Averager", Dl4jParameterAverager.class);
+    DefaultDelayOperator delay = dag.addOperator("Delay", DefaultDelayOperator.class);
+
+    RoundRobinStreamCodec rrCodec = new RoundRobinStreamCodec();
+    rrCodec.setN(numWorkers);
+
+    dag.setOperatorAttribute(Worker, Context.OperatorContext.PARTITIONER, new StatelessPartitioner<Dl4jWorkerOperator>(numWorkers));
+    dag.setInputPortAttribute(Worker.dataPort, Context.PortContext.STREAM_CODEC, rrCodec);
+
+    dag.addStream("Data:InputData-Master", inputData.outputData, Master.dataPort);
+    dag.addStream("Data:Master-Worker", Master.outputData, Worker.dataPort);
+    dag.addStream("Parameters:Master-Worker", Master.newParameters, Worker.controlPort);
+    dag.addStream("Parameters:Worker-ParameterAverager", Worker.output, ParameterAverager.inputPara);
+    dag.addStream("Parameters:ParameterAverager-Delay", ParameterAverager.outputPara, delay.input);
+    dag.addStream("Parameters:Delay-Master", delay.output, Master.finalParameters);
+
+    //    DL4j Configurations
+
+    MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+      .seed(123)
+      .iterations(2)
+      .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+      .learningRate(0.0015)
+      .updater(Updater.NESTEROVS).momentum(0.9)
+      .list()
+      .layer(0, new DenseLayer.Builder().nIn(4).nOut(10)
+        .weightInit(WeightInit.XAVIER)
+        .activation(Activation.RELU)
+        .build())
+      .layer(1, new DenseLayer.Builder().nIn(10).nOut(20)
+        .weightInit(WeightInit.XAVIER)
+        .activation(Activation.RELU)
+        .build())
+      .layer(2, new DenseLayer.Builder().nIn(20).nOut(10)
+        .weightInit(WeightInit.XAVIER)
+        .activation(Activation.SIGMOID)
+        .build())
+      .layer(3, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+        .weightInit(WeightInit.XAVIER)
+        .activation(Activation.SOFTMAX).weightInit(WeightInit.UNIFORM)
+        .nIn(10).nOut(3).build())
+      .pretrain(false).backprop(true).build();
+
+    Master.setConf(conf);
+    Worker.setConf(conf);
+
+    ParameterAverager.setNumWorkers(numWorkers);
+
+  }
+}
