dc4f0507b54244890be888aab0676002a511e164
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-04-19 14:20:06 +0530

    Separate App Directory for MNIST example

---
+++ src/main/java/com/github/ambarishpande/MnistExample/LineTokenizer.java
@@ -0,0 +1,89 @@
+package com.github.ambarishpande.MnistExample;
+
+/**
+ * Created by devraj on 24/2/17.
+ */
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+
+import com.github.ambarishpande.MasterWorkerModule.DataSetWrapper;
+
+import com.datatorrent.api.Context;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.common.util.BaseOperator;
+//import org.nd4j.linalg.dataset.Dataset;
+
+public class LineTokenizer extends BaseOperator
+{
+
+  private int tuplesPerWindow = 5;
+  public final transient DefaultOutputPort<DataSetWrapper> output = new DefaultOutputPort<>();
+  //public final transient DefaultOutputPort<String> outputNegative = new DefaultOutputPort<>();
+
+  public final transient DefaultInputPort<String> input = new DefaultInputPort<String>()
+  {
+
+    @Override
+    public void process(String line)
+    {
+      // TODO Auto-generated method stub
+
+
+
+        String csvsplit = ",";
+        String[] label1 = line.split(csvsplit);
+
+        int results = label1.length;
+        double data[] = new double[784];
+        for (int i = 1; i < results; i++) {
+          try {
+            data[i - 1] = Double.parseDouble(label1[i]);
+            if(data[i-1] > 35)
+            {
+              data[i-1] = 1;
+            }
+            else
+            {
+              data[i-1] = 0;
+            }
+          } catch (NumberFormatException nfe) {
+            //NOTE: write something here if you need to recover from formatting errors
+          }
+          ;
+        }
+
+        double lab[] = new double[10];
+
+        lab[Integer.parseInt(label1[0])] = 1.00;
+        INDArray label = Nd4j.create(lab, new int[]{1, 10});
+//            System.out.println("Label is " + label);
+
+        INDArray arr = Nd4j.create(data, new int[]{1, 784});
+//            System.out.println("Data is :" + arr);
+        DataSet d = new DataSet(arr, label);
+///            System.out.print(d);
+        DataSetWrapper dw = new DataSetWrapper(d);
+        output.emit(dw);
+
+
+
+    }
+
+  };
+
+  @Override
+  public void setup(Context.OperatorContext context)
+  {
+
+  }
+
+  public void beginWindow(long windowId)
+  {
+
+  }
+
+}
+
---
+++ src/main/java/com/github/ambarishpande/MnistExample/MnistExample.java
@@ -0,0 +1,105 @@
+package com.github.ambarishpande.MnistExample;
+
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import org.apache.hadoop.conf.Configuration;
+
+import com.github.ambarishpande.MasterWorkerModule.Dl4jEvaluatorOperator;
+import com.github.ambarishpande.MasterWorkerModule.Dl4jMasterOperator;
+import com.github.ambarishpande.MasterWorkerModule.Dl4jParameterAverager;
+import com.github.ambarishpande.MasterWorkerModule.Dl4jWorkerOperator;
+import com.github.ambarishpande.MasterWorkerModule.ModelSaverOperator;
+import com.github.ambarishpande.MasterWorkerModule.RoundRobinStreamCodec;
+
+import com.datatorrent.api.Context;
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.annotation.ApplicationAnnotation;
+import com.datatorrent.common.partitioner.StatelessPartitioner;
+import com.datatorrent.common.util.DefaultDelayOperator;
+
+/**
+ * Created by hadoopuser on 23/3/17.
+ */
+@ApplicationAnnotation(name="MnistExample")
+public class MnistExample implements StreamingApplication
+{
+  private MultiLayerConfiguration conf;
+  private int numWorkers;
+
+  @Override
+  public void populateDAG(DAG dag, Configuration configuration)
+  {
+
+    numWorkers = Integer.parseInt(configuration.get("dt.application.MnistExample.operator.ParameterAverager.prop.numWorkers"));;
+//      Add all operators
+    FileInputOp inputData = dag.addOperator("fileInput", FileInputOp.class);
+    LineTokenizer tokenizer = dag.addOperator("Tokenized Data", LineTokenizer.class);
+    Dl4jMasterOperator Master = dag.addOperator("Master", Dl4jMasterOperator.class);
+    Dl4jWorkerOperator Worker = dag.addOperator("Worker", Dl4jWorkerOperator.class);
+    Dl4jParameterAverager ParameterAverager = dag.addOperator("Parameter Averager", Dl4jParameterAverager.class);
+    DefaultDelayOperator delay = dag.addOperator("Delay", DefaultDelayOperator.class);
+//    Dl4jEvaluatorOperator eval = dag.addOperator("Eval",Dl4jEvaluatorOperator.class);
+//    ModelSaverOperator saver = dag.addOperator("Saver",ModelSaverOperator.class);
+
+    RoundRobinStreamCodec rrCodec = new RoundRobinStreamCodec();
+    rrCodec.setN(numWorkers);
+
+//    Set Operator Attributes
+    dag.setOperatorAttribute(Worker, Context.OperatorContext.PARTITIONER, new StatelessPartitioner<Dl4jWorkerOperator>(numWorkers));
+    dag.setInputPortAttribute(Worker.dataPort, Context.PortContext.STREAM_CODEC,rrCodec);
+
+//    Add all Streams
+    dag.addStream("Data:Input-Tokenizer", inputData.output, tokenizer.input).setLocality(DAG.Locality.CONTAINER_LOCAL);
+    dag.addStream("Data:Tokenizer-Master",tokenizer.output, Master.dataPort);
+    dag.addStream("Data:Master-Worker", Master.outputData, Worker.dataPort);
+    dag.addStream("Parameters:Master-Worker", Master.newParameters, Worker.controlPort);
+    dag.addStream("Parameters:Worker-ParameterAverager", Worker.output, ParameterAverager.inputPara);
+    dag.addStream("Parameters:ParameterAverager-Delay", ParameterAverager.outputPara, delay.input);
+    dag.addStream("Parameters:Delay-Master", delay.output, Master.finalParameters);
+//    dag.addStream("Model:Master-Evaluator",Master.modelOutput,eval.modelInput);
+//    dag.addStream("Parameters:Master-Saver",Master.modelOutput,saver.modelInput);
+//    DL4j Configurations
+
+    conf = new NeuralNetConfiguration.Builder()
+      .seed(123)
+      .iterations(2)
+      .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+      .learningRate(0.006)
+      .updater(Updater.NESTEROVS).momentum(0.9)
+      .regularization(true).l2(1e-4)
+      .list()
+      .layer(0, new DenseLayer.Builder() //create the first, input layer with xavier initialization
+        .nIn(28 * 28)
+        .nOut(1000)
+        .activation(Activation.RELU)
+        .weightInit(WeightInit.XAVIER)
+        .build())
+      .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD) //create hidden layer
+        .nIn(1000)
+        .nOut(10)
+        .activation(Activation.SOFTMAX)
+        .weightInit(WeightInit.XAVIER)
+        .build())
+      .pretrain(false).backprop(true) //use backpropagation to adjust weights
+      .build();
+
+
+    Master.setConf(conf);
+    Worker.setConf(conf);
+
+//    ParameterAverager.setNumWorkers(numWorkers);
+//    Worker.setBatchSize(32);
+//    saver.setConf(conf);
+//    saver.setSaveLocation("/user/hadoopuser/mnist/");
+//    saver.setFilename("mnist-1.zip");
+  }
+}
