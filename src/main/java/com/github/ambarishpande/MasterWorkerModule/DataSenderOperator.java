a5cb3380348811212d232d2158b25fea31c96196
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-04-19 13:41:23 +0530

    Removed unnecesary logs

--- src/main/java/com/github/ambarishpande/MasterWorkerModule/DataSenderOperator.java
+++ src/main/java/com/github/ambarishpande/MasterWorkerModule/DataSenderOperator.java
@@ -26,37 +26,29 @@ public class DataSenderOperator implements InputOperator
   private DataSetIterator dataSetIterator;
   private int numEpochs;
   public transient DefaultOutputPort<DataSetWrapper> outputData = new DefaultOutputPort<DataSetWrapper>();
-  public transient DefaultOutputPort<Integer> controlPort  = new DefaultOutputPort<>();
-  private int batchSize;
+//  private int batchSize;
   @FieldSerializer.Bind(JavaSerializer.class)
   private DataSet d;
-  private  int count;
 
   @Override
   public void emitTuples()
   {
     try{
 
-      if(batchSize>0)
-      {
         if (dataSetIterator.hasNext())
         {
           d = dataSetIterator.next();
-//      LOG.info("Sending Data : " + d.toString());
+
           DataSetWrapper dw = new DataSetWrapper(d);
           outputData.emit(dw);
-          count++;
-          batchSize--;
+
         }else{
           numEpochs--;
           if(numEpochs > 0)
           {
-//        Temporary solution for multiple epochs
-            LOG.info("Epoch number " + numEpochs + "Complete");
-            controlPort.emit(1);
             dataSetIterator.reset();
           }
-        }
+
 
       }
 
@@ -72,26 +64,22 @@ public class DataSenderOperator implements InputOperator
   @Override
   public void beginWindow(long l)
   {
-    LOG.info("Begin Window in Sender...");
-    batchSize = 5;
-    count = 0;
+
   }
 
   @Override
   public void endWindow()
   {
-    LOG.info("Emmited" + count + " tuples in window.");
   }
 
   @Override
   public void setup(Context.OperatorContext context)
   {
-    dataSetIterator = new IrisDataSetIterator(10, 150);
+    dataSetIterator = new IrisDataSetIterator(1, 150);
     numEpochs = 2000;
-    count = 0;
-    LOG.info("Number of examples  :" + dataSetIterator.numExamples());
+//    LOG.info("Number of examples  :" + dataSetIterator.numExamples());
     LOG.info("iris dataset loaded...");
-    d = new DataSet();
+
 
   }
 
