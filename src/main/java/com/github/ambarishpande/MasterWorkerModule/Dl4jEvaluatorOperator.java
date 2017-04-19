f6a6ad73e606a32c0724b8c656c8e91a10b49d1b
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-04-19 13:43:56 +0530

    Removed unnecesary imports

--- src/main/java/com/github/ambarishpande/MasterWorkerModule/Dl4jEvaluatorOperator.java
+++ src/main/java/com/github/ambarishpande/MasterWorkerModule/Dl4jEvaluatorOperator.java
@@ -1,23 +1,12 @@
 package com.github.ambarishpande.MasterWorkerModule;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 
 import com.esotericsoftware.kryo.serializers.FieldSerializer;
 import com.esotericsoftware.kryo.serializers.JavaSerializer;
@@ -25,11 +14,7 @@ import com.esotericsoftware.kryo.serializers.JavaSerializer;
 import com.datatorrent.api.Context;
 import com.datatorrent.api.DefaultInputPort;
 import com.datatorrent.common.util.BaseOperator;
-
-import java.util.Queue;
 import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created by hadoopuser on 25/1/17.
@@ -41,7 +26,6 @@ public class Dl4jEvaluatorOperator extends BaseOperator
   private transient volatile boolean execute;
   private static int DEFAULT_QUEUE_CAPACITY = 4 * 1024;
   private int queueCapacity = DEFAULT_QUEUE_CAPACITY;
-  private transient ScheduledExecutorService scanService;
   protected transient LinkedBlockingDeque<ApexMultiLayerNetwork> emitQueue;
   private static final Logger LOG = LoggerFactory.getLogger(Dl4jEvaluatorOperator.class);
   @FieldSerializer.Bind(JavaSerializer.class)
@@ -56,65 +40,45 @@ public class Dl4jEvaluatorOperator extends BaseOperator
       emitQueue.add(model);
 
     }
-   };
+  };
 
-    public void setup(Context.OperatorContext context)
+  public void setup(Context.OperatorContext context)
+  {
+    lastAccuracy = 0.0;
+    dataSetIterator = new IrisDataSetIterator(1, 150);
+    emitQueue = new LinkedBlockingDeque<>(queueCapacity);
+    execute = true;
+    Eval e = new Eval();
+
+    Thread t = new Thread(e);
+    t.start();
+
+  }
+
+  public class Eval implements Runnable
+  {
+    ApexMultiLayerNetwork model;
+
+    @Override
+    public void run()
     {
-      lastAccuracy = 0.0;
-      dataSetIterator = new IrisDataSetIterator(5, 150);
-      emitQueue = new LinkedBlockingDeque<>(queueCapacity);
-      execute = true;
-      Eval e = new Eval();
-
-      Thread t = new Thread(e);
-      t.start();
-
-    }
-
-    public class Eval implements Runnable
-    {
-      ApexMultiLayerNetwork model;
-      @Override
-      public void run()
-      {
-        while (execute) {
-          if(!emitQueue.isEmpty())
-          {
-            LOG.info("Queue size : " + emitQueue.size());
-            model = emitQueue.removeFirst();
-            LOG.info("Evaluating model " + model.getModel().params().toString());
+      while (execute) {
+        if (!emitQueue.isEmpty()) {
+          LOG.info("Queue size : " + emitQueue.size());
+          model = emitQueue.removeFirst();
+          LOG.info("Evaluating model " + model.getModel().params().toString());
           dataSetIterator.reset();
-          Evaluation eval = new Evaluation(3); //create an evaluation object with 10 possible classes
+          Evaluation eval = new Evaluation(3); //create an evaluation object with 3 possible classes
           while (dataSetIterator.hasNext()) {
             DataSet next = dataSetIterator.next();
             INDArray output = model.getModel().output(next.getFeatureMatrix()); //get the networks prediction
             eval.eval(next.getLabels(), output); //check the prediction against the true class
-//            double accuracy = eval.accuracy();
-//            if (accuracy > lastAccuracy) {
-//              lastAccuracy = accuracy;
-//              Configuration configuration = new Configuration();
-//
-//              try {
-//                LOG.info("Trying to save model...");
-//                FileSystem hdfs = FileSystem.newInstance(new URI("hdfs://master:54310/"), configuration);
-//                FSDataOutputStream hdfsStream = hdfs.create(new Path("/user/hadoopuser/iris.zip"));
-//                ModelSerializer.writeModel(model.getModel(), hdfsStream, true);
-//                LOG.info("Model saved to location");
-//
-//              } catch (IOException e) {
-//                e.printStackTrace();
-//              } catch (URISyntaxException e) {
-//                e.printStackTrace();
-//              }
-//
-//            }
-          //  LOG.info(eval.stats());
           }
-            LOG.info(eval.stats());
-          }
-
+          LOG.info(eval.stats());
         }
+
       }
     }
-
   }
+
+}
