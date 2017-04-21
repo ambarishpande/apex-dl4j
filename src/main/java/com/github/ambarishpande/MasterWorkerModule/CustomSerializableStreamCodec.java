c31f8e432ceefe1b5342a300fc3a0d599cbd05e1
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-04-21 14:51:12 +0530

    Cutom stream codec for delegating serialization of MLN to Java Serializer

--- src/main/java/com/github/ambarishpande/MasterWorkerModule/CustomSerializableStreamCodec.java
+++ src/main/java/com/github/ambarishpande/MasterWorkerModule/CustomSerializableStreamCodec.java
@@ -1,8 +1,23 @@
 package com.github.ambarishpande.MasterWorkerModule;
 
-/**
- * Created by hadoopuser on 21/4/17.
- */
-public class CustomSerializableStreamCodec
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.UUID;
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
+
+public class CustomSerializableStreamCodec<T> extends com.datatorrent.lib.codec.KryoSerializableStreamCodec<T>
 {
+  private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+  {
+    in.defaultReadObject();
+    this.kryo = new Kryo();
+    this.kryo.setClassLoader(Thread.currentThread().getContextClassLoader());
+    this.kryo.register(MultiLayerNetwork.class, new JavaSerializer()); // Register the types along with custom serializers
+  }
+
+  private static final long serialVersionUID = 201411031405L;
 }
