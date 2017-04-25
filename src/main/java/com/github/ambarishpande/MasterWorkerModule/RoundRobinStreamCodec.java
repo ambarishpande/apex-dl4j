63df22686a8008805c492176c791bd6aff5b10f2
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-04-25 16:29:57 +0530

    Added readObject Method to specify Java Serializer for Nd4j types.

--- src/main/java/com/github/ambarishpande/MasterWorkerModule/RoundRobinStreamCodec.java
+++ src/main/java/com/github/ambarishpande/MasterWorkerModule/RoundRobinStreamCodec.java
@@ -1,5 +1,15 @@
 package com.github.ambarishpande.MasterWorkerModule;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.serializers.JavaSerializer;
+
 import com.datatorrent.lib.codec.KryoSerializableStreamCodec;
 
 /**
@@ -18,5 +28,13 @@ public class RoundRobinStreamCodec extends KryoSerializableStreamCodec<Object>
   public void setN(int n)
   {
     nPartitions = n;
+  }
+
+  private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+  {
+    in.defaultReadObject();
+    this.kryo = new Kryo();
+    this.kryo.setClassLoader(Thread.currentThread().getContextClassLoader());
+    this.kryo.register(DataSet.class, new JavaSerializer());
   }
 } 
