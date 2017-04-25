56d38dee8922b2d02a1a6f753e1e452eef539b11
Ambarish Pande <ambarish.pande2011@gmail.com>
2017-04-25 16:29:13 +0530

    Registered DataSet and INDArray to kryo

--- src/main/java/com/github/ambarishpande/MasterWorkerModule/CustomSerializableStreamCodec.java
+++ src/main/java/com/github/ambarishpande/MasterWorkerModule/CustomSerializableStreamCodec.java
@@ -5,6 +5,8 @@ import java.io.ObjectInputStream;
 import java.util.UUID;
 
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
 
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.serializers.JavaSerializer;
@@ -17,6 +19,8 @@ public class CustomSerializableStreamCodec<T> extends com.datatorrent.lib.codec.
     this.kryo = new Kryo();
     this.kryo.setClassLoader(Thread.currentThread().getContextClassLoader());
     this.kryo.register(MultiLayerNetwork.class, new JavaSerializer()); // Register the types along with custom serializers
+    this.kryo.register(DataSet.class, new JavaSerializer());
+    this.kryo.register(INDArray.class, new JavaSerializer());
   }
 
   private static final long serialVersionUID = 201411031405L;
